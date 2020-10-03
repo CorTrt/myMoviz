6c5216ce79f45a93bf968edd333a4e8e65ca1ce4
CorTrt <trt.corentin@gmail.com>
2020-10-03 10:39:23 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,187 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+
+import { 
+  Container, 
+  Row, 
+  Col,
+  Collapse,
+  Navbar,
+  Nav,
+  NavItem,
+  NavLink,
+  Popover,
+  PopoverHeader,
+  PopoverBody,
+  ListGroup,
+  ListGroupItem
+} from 'reactstrap';
+
+import { 
+  Button,
+  Image
+} from 'react-bootstrap';
+
+import Moviz from './moviz'
+
 import './App.css';
 
 function App() {
+
+  const moviesDatas = [
+    {
+      name: 'Frozen',
+      desc: 'Un film glaçant.',
+      img: '/img/frozen.jpg',
+      note: 6.7,
+      vote: 7
+    },
+    {
+      name: 'BadBoy 3',
+      desc: 'Un film méchant.',
+      img: '/img/badboy3.jpg',
+      note: 5.3,
+      vote: 4
+    },
+    {
+      name: 'Jumanji',
+      desc: 'Un film sauvage.',
+      img: '/img/jumanji.jpg',
+      note: 4.8,
+      vote: 6
+    },
+    {
+      name: 'Maléfique',
+      desc: 'Un film maléfique.',
+      img: '/img/maleficent.jpg',
+      note: 6.4,
+      vote: 11
+    },
+    {
+      name: 'Il était une fois',
+      desc: 'Un film casse-cou.',
+      img: '/img/once_upon.jpg',
+      note: 5.8,
+      vote: 9
+    },
+    {
+      name: 'Star-Wars',
+      desc: 'Un film fort.',
+      img: '/img/starwars.jpg',
+      note: 10,
+      vote: 16
+    },
+    {
+      name: 'Terminator',
+      desc: 'Un film mécanique.',
+      img: '/img/terminator.jpg',
+      note: 6.3,
+      vote: 5
+    },
+  ]
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [movieLikeList, setMovieLikeList] = useState([]);
+
+  const [popoverOpen, setPopoverOpen] = useState(false);
+  const toggle = () => setPopoverOpen(!popoverOpen);
+
+  const [liked, setLiked] = useState(false);
+
+
+  /* Ajouter un film */
+  let addToList = (movieDatas) => {
+      setMovieLikeList([...movieLikeList, {name: movieDatas.name, img: movieDatas.img}]);
+      setLiked(true);
+  }
+
+  let delFromList = (movieDatas) => {
+          setMovieLikeList([...movieLikeList.filter((e) => e.name !== movieDatas.name)]);
+          setLiked(false);
+  }
+
+
+  const moviesList = moviesDatas.map((movie, i) => {
+
+    var result = movieLikeList.find(element => element.name == movie.name);
+    var isSee = false
+    if(result != undefined){
+      isSee = true
+    }
+
+    return(
+      <Moviz addListParent={addToList} delListParent={delFromList} liked={isSee} movieName={movie.name} movieDesc={movie.desc} movieImg={movie.img} movieNote={movie.note} movieVote={movie.vote} />
+    )
+  })
+
+  const wishListItems = movieLikeList.map((movie, i) => {
+    return(
+      <ListGroupItem>
+        <div className='d-flex justify-content-center align-items-center'>
+          <div>
+            <Image style={{height:'5em', width:'5em', marginRight:'1em'}} src={movie.img} roundedCircle />{movie.name}
+          </div>
+
+          <FontAwesomeIcon onClick={()=>delFromList({name: movie.name, img: movie.img})} style={{marginLeft:'1em'}} icon={faTimesCircle} />
+        </div>
+      </ListGroupItem>
+    )
+  }) 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div style={{backgroundColor:'#333333'}}>
+      <Container>
+
+        <Row>
+            <Col>
+
+            <div>
+                <Navbar color="dark" light expand="md">
+                  <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto d-flex align-items-center justify-content-center" navbar>
+                      
+                      <NavItem>
+                        <NavLink href="/components/">
+                            <img src='/img/logo.png' alt='logo' />
+                        </NavLink>
+                      </NavItem>
+
+                      <Button id='Popover1' type='button' style={{height:'50%', marginLeft: '5em'}} variant="outline-warning">{movieLikeList.length} Films likés</Button>
+                    
+                      <Popover placement="bottom" isOpen={popoverOpen} target="Popover1" toggle={toggle}>
+                        <PopoverHeader>Like List</PopoverHeader>
+                          <PopoverBody>
+                            <ListGroup variant='flush'>
+                              {wishListItems.length === 0 ? <p>Il faut ajouter un film ;)</p> : wishListItems}
+                            </ListGroup>
+                          </PopoverBody>
+                      </Popover>
+                      
+                    </Nav>
+                  </Collapse>
+                </Navbar>
+              </div>
+            
+            </Col>
+        </Row>
+
+        <Row style={{marginTop:30}}>
+            
+              {moviesList}
+              
+        </Row>
+
+
+      </Container>
     </div>
+
   );
+
 }
 
 export default App;
---
+++ src/moviz.js
@@ -0,0 +1,126 @@
+import React, { useEffect, useState } from 'react';
+
+import {  
+  Col,
+  Card,
+  CardImg,
+  CardBody,
+  CardText,
+} from 'reactstrap';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faStar, faVideo } from '@fortawesome/free-solid-svg-icons';
+
+
+function Moovie(props) {
+  
+  const [likeMovie, setLikeMovie] = useState(props.liked);
+  const [colorLike, setColorLike] = useState('black');
+  const [watchMovie, setWatchMovie] = useState(false);
+  const [myRatingMovie, setMyRatingMovie] = useState(5);
+  const [countWatchMouvie, setCountWatchMouvie] = useState(0);
+  const [globalRatingMovie, setGlobalRatingMovie] = useState(props.movieNote);
+  const [countRating, setCountRating] = useState(props.movieVote);
+  const [alreadyVoted, setAlreadyVoted] = useState(false);
+
+  console.log('PROPS.LIKED', props.liked)
+
+  let clickLike = () => {
+    // setLikeMovie(!likeMovie)
+    if(likeMovie === false){
+      setColorLike('#e74c3C')
+    } else {
+      setColorLike('black')
+    }
+  }
+
+  let clickWatch = () => {
+    setWatchMovie(true);
+    setCountWatchMouvie(countWatchMouvie+1)
+  }
+
+  /* Modifie la note de l'utilisateur avec MouseHover (avant le clique) */
+  let mouseHoverRating = (id) => {
+    if(alreadyVoted === false){
+      setMyRatingMovie(id)
+    }
+  }
+
+  /* Valide la note de l'utilisateur */
+  let clickRating = (id) => {
+    setCountRating(props.movieVote+1)
+    setAlreadyVoted(true);
+    setMyRatingMovie(id)
+  }
+
+  let likeListParent = (movieDatas) => {
+
+    if(props.liked === false){
+      props.addListParent(movieDatas)
+    }
+    if(props.liked === true){
+      props.delListParent(movieDatas)
+    }
+
+  }
+
+
+  /* Calcul note moyenne du film */
+  let totalRating = ( (globalRatingMovie+myRatingMovie)/(2) )
+
+  let myRatingStars = [];
+  for(let i=0;i<10;i++){
+      var starColor = 'black'
+      if(alreadyVoted === false){
+        let action = 'onMouseOver'
+      } else {
+        let action = 'onClick'
+      }
+    if(i<myRatingMovie){
+      starColor = '#f1c40f'
+    }
+    myRatingStars.push(<FontAwesomeIcon onMouseOver={()=> mouseHoverRating(i+1)} onClick={()=> clickRating(i+1)} style={{color: starColor}} icon={faStar} />)
+  }
+
+  var globalRatingStars = [];
+  for(let i=0;i<10;i++){
+    var starColor = 'black'
+    if(i<totalRating){
+      starColor = '#f1c40f'
+    }
+    globalRatingStars.push(<FontAwesomeIcon style={{color: starColor}} icon={faStar} />)
+  }
+
+
+    return(
+
+        <Col xs='12' lg='6' xl='4'>
+            
+                <div style={{marginBottom:10}}>
+
+                    <Card>
+                      <CardImg top width="100%" src={props.movieImg} alt="Frozen" />
+                      <CardBody>
+                        <CardText style={{marginTop:5, marginBottom:5}}> Likes <FontAwesomeIcon onClick={ ()=> {likeListParent( {name: props.movieName, img: props.movieImg} ); clickLike()} } style={{cursor: 'pointer', color: colorLike}} icon={faHeart} /> </CardText>
+                        <CardText style={{marginTop:5, marginBottom:5}}> Nombre de vues 
+                          <FontAwesomeIcon onClick={()=> clickWatch()} style={{cursor: 'pointer', marginLeft: "1em", marginRight: "0.5em"}} icon={faVideo} />
+                          ({countWatchMouvie})
+                        </CardText>
+                        <CardText style={{marginTop:5, marginBottom:5}}> Mon avis
+                          {myRatingStars}
+                          ({myRatingMovie})
+                        </CardText>
+                        <CardText style={{marginTop:5, marginBottom:5}}>Moyenne {globalRatingStars} ({countRating})</CardText>
+                        <CardText style={{marginTop:5, marginBottom:5}}><h3>{props.movieName}</h3></CardText>
+                        <CardText style={{marginTop:5, marginBottom:5}}><h6>{props.movieDesc}</h6></CardText>
+                      </CardBody>
+                    </Card>
+                  </div>
+              
+              </Col>
+
+    )
+
+}
+
+export default Moovie;
